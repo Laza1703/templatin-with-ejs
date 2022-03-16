bf1e2fbdeeaf5e714ab797c86c35ec32fa6726af
Lazar Matic <91702100+Laza1703@users.noreply.github.com>
2022-03-16 18:29:11 +0000

    done

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.set('view engine','ejs');
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
 
+app.set('view engine','ejs'); 
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+var data = require('./data/test.json');
 app.get('/', (req, res) => {
   var title = 'My Home Page';
-  res.render('index',{title:title});
+  res.render('pages/index',{title:title});
+});
+
+app.get('/dogs', (req, res) => {
+  var title = 'Dogs';
+  res.render('pages/dogs',{title:title});
+});
+
+app.get('/cats', (req, res) => {
+  var title = 'Cats';
+  res.render('pages/cats',{title:title});
+});
+
+app.get('/rabbits', (req, res) => {
+  var title = 'Rabbits';
+  res.render('pages/rabbits',{title:title});
+});
+
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
 });
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log('Example app listening on port ${port}');
+  console.log(data);
 });
